48ac480459945f16a130bca0ebadb735e52352dd
Herbert <herbert@Aarons-MBP.fritz.box>
2020-05-01 17:13:26 +0200

    ssh config test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Header from "./Components/Header";
 
-export default App;
+export default () => {
+  return <Header />;
+};
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <span>About me</span>
+      <span>Work</span>
+    </header>
+  );
+};
+
+export default Header;
